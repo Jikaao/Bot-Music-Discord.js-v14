0a764e10d858aad04e49157e3ee9bb39638db180
JordanB <89873209+Jikaao@users.noreply.github.com>
2023-08-20 20:08:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,28 @@ const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 const fs = require("fs");
 const { Player } = require("discord-player");
+const { ActivityType } = require("discord.js")
 
 dotenv.config();
 const TOKEN = process.env.TOKEN;
 
 const LOAD_SLASH = process.argv[2] == "load";
 
-const CLIENT_ID = "";   //Enter ID//
-const GUILD_ID = "";   //Enter ID//
+const CLIENT_ID = ""; //ID BOT
+const GUILD_ID = ""; //ID SERVEUR
 
-const client = new Discord.Client({
-  intents: ["GUILDS", "GUILD_VOICE_STATES"],
+const { Client, Intents } = require('discord.js');
+
+const botclient = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildVoiceStates,
+        // ... d'autres intents
+    ]
 });
 
-client.slashcommands = new Discord.Collection();
-client.player = new Player(client, {
+botclient.slashcommands = new Discord.Collection();
+botclient.player = new Player(botclient, {
   ytdlOptions: {
     quality: "highestaudio",
     highWaterMark: 1 << 25,
@@ -32,14 +39,13 @@ const slashFiles = fs
   .filter((file) => file.endsWith(".js"));
 for (const file of slashFiles) {
   const slashcmd = require(`./slash/${file}`);
-  client.slashcommands.set(slashcmd.data.name, slashcmd);
+  botclient.slashcommands.set(slashcmd.data.name, slashcmd);
   if (LOAD_SLASH) commands.push(slashcmd.data.toJSON());
 }
 
-
 if (LOAD_SLASH) {
   const rest = new REST({ version: "9" }).setToken(
-    "TOKEN" //enter token here//
+    "TOKEN" // ENTRER LE TOKEN ICI
   );
   console.log("Deploying slash commands");
   rest
@@ -57,24 +63,24 @@ if (LOAD_SLASH) {
       }
     });
 } else {
-  client.on("ready", () => {
-    client.user.setActivity(`Fairy Tail`, { type: "WATCHING", }
-    );
-
-    console.log(`Logged in as ${client.user.tag}`);
+  botclient.on("ready", () => {
+    botclient.user.setActivity(`Shiru tomber par terre`, { type: ActivityType.Watching, url: "https://discord.gg/FairyTailFR" });  //Watching/play/listeren/steaming
+    botclient.user.setStatus("dnd"); //"dnd"/"online"/"offline"/"idle"
+    console.log(`Logged in as ${botclient.user.tag}`);
   });
-  client.on("interactionCreate", (interaction) => {
+  botclient.on("interactionCreate", (interaction) => {
     async function handleCommand() {
       if (!interaction.isCommand()) return;
 
-      const slashcmd = client.slashcommands.get(interaction.commandName);
+      const slashcmd = botclient.slashcommands.get(interaction.commandName);
       if (!slashcmd) interaction.reply("Commande slash non valide");
 
       await interaction.deferReply();
-      await slashcmd.run({ client, interaction });
+      await slashcmd.run({ client: botclient, interaction });
     }
     handleCommand();
   });
 
-  client.login("");  //enter client login//
+  botclient.login("TOKEN"); // ENTRER LE TOKEN ICI
 }
+
