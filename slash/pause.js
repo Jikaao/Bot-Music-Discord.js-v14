1c19d9147539fe15f145b149f9d0816b7e667be5
JordanB <89873209+Jikaao@users.noreply.github.com>
2023-09-12 21:04:03 +0200

    Update pause.js

--- slash/pause.js
+++ slash/pause.js
@@ -5,7 +5,7 @@ module.exports = {
 	run: async ({ client, interaction }) => {
 		const queue = client.player.getQueue(interaction.guildId)
 
-		if (!queue) return await interaction.editReply("Il n'y a pas de chansons dans la file d'attente")
+		if (!queue) return await interaction.editReply("Il n'y a aucune musique dans la file d'attente")
 
 		queue.setPaused(true)
         await interaction.editReply("La musique a été mise en pause ! Utilisez `/start` pour reprendre la musique")
