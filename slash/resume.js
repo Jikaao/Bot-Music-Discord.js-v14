8f7bbed47aff13c10c2a7d3cbcb733b1aeaa3761
JordanB <89873209+Jikaao@users.noreply.github.com>
2023-10-21 20:22:58 +0200

    Update resume.js

--- slash/resume.js
+++ slash/resume.js
@@ -5,7 +5,7 @@ module.exports = {
 	run: async ({ client, interaction }) => {
 		const queue = client.player.getQueue(interaction.guildId)
 
-		if (!queue) return await interaction.editReply("Il n'y a pas de musique dans la file d'attente")
+		if (!queue) return await interaction.editReply("Il n'y a aucune musique dans la file d'attente")
 
 		queue.setPaused(false)
         await interaction.editReply("La musique a été relancé ! Utilisez `/pause` pour mettre la musique en pause.")
